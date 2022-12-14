05212ae6198b4cc22b8c37d37eb063cac1527ba4
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2022-12-14 12:53:13 +0400

    Initial Commit

---
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by Konstantin Bolgar-Danchenko on 14.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
