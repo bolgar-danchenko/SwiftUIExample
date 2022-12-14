eb1cf547243f510954a695abd376f15320901957
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2022-12-14 14:06:22 +0400

    Tasks 1 and 2 done

---
+++ SwiftUIExample/LoginView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIExample
+//
+//  Created by Konstantin Bolgar-Danchenko on 14.12.2022.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    var body: some View {
+    
+        Text("Hello, world!")
+     
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ SwiftUIExample/MainView.swift
@@ -0,0 +1,36 @@
+//
+//  MainView.swift
+//  SwiftUIExample
+//
+//  Created by Konstantin Bolgar-Danchenko on 14.12.2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Lesson Code", systemImage: "swift")
+                }
+            LoginView()
+                .tabItem {
+                    Label("Login", systemImage: "person")
+                }
+            TableView()
+                .tabItem {
+                    Label("Table", systemImage: "tablecells")
+                }
+            
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+
+    }
+}
